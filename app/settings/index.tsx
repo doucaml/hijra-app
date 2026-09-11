e64043dcc407162fa570e19c28966b2935c3588c
Mohamed Doucouré <mohamed.doucoure@protonmail.com>
2026-09-11 10:12:18 +0000

    feat(settings): persist user preferences

--- app/settings/index.tsx
+++ app/settings/index.tsx
@@ -1,18 +1,51 @@
 import { Href, Link } from "expo-router";
-import { useState } from "react";
 import { View, Text, Pressable, Switch } from "react-native";
+import { createMMKV, useMMKVBoolean, useMMKVNumber } from "react-native-mmkv";
+
+const storage = createMMKV();
+storage.contains("preferences.notifications") === undefined &&
+  storage.set("preferences.notifications", false);
+storage.getNumber("preferences.days_correction") === undefined &&
+  storage.set("preferences.days_correction", 0);
+
+const usePreferences = () => {
+  const [notificationState, setNotificationState] = useMMKVBoolean(
+    "preferences.notifications",
+  );
+
+  const [daysCorrection, setDaysCorrection] = useMMKVNumber(
+    "preferences.days_correction",
+  );
+
+  const toggleNotificationState = () => setNotificationState((prev) => !prev);
+  const editDaysCorrection = () =>
+    setDaysCorrection((prev) => {
+      if (prev === 2) return -2;
+      else return prev! + 1;
+    });
+  return {
+    notificationState,
+    toggleNotificationState,
+    daysCorrection,
+    editDaysCorrection,
+  };
+};
+
+function ScreenLink({ href, title }: { href: Href; title: string }) {
+  return (
+    <Link href={href} className="p-3 my-1 rounded-lg bg-gray-200">
+      <Text>{title}</Text>
+    </Link>
+  );
+}
 
 export default function Screen() {
-  const [notificationsValue, setNotificationValue] = useState(false);
-
-  const [hijraDateCorrection, setHijraDateCorrection] = useState(0);
-
-  const onDateCorrectionPress = () => {
-    setHijraDateCorrection((prev) => {
-      if (prev === 2) return -2;
-      else return prev + 1;
-    });
-  };
+  const {
+    notificationState,
+    toggleNotificationState,
+    daysCorrection,
+    editDaysCorrection,
+  } = usePreferences();
 
   return (
     <View className="flex-1 gap-y-6">
@@ -24,8 +57,8 @@ export default function Screen() {
         <View className="flex-row items-center justify-between p-2 my-1 h-12 rounded-lg bg-gray-200">
           <Text>Allow notifications</Text>
           <Switch
-            value={notificationsValue}
-            onValueChange={(newValue) => setNotificationValue(newValue)}
+            value={notificationState}
+            onValueChange={toggleNotificationState}
           />
         </View>
 
@@ -34,12 +67,12 @@ export default function Screen() {
 
           <View className="flex-row gap-x-4 ">
             <Pressable
-              onPress={onDateCorrectionPress}
+              onPress={editDaysCorrection}
               className="flex-row items-center justify-between px-2 w-17 h-8 shadow bg-gray-100 rounded-lg"
             >
               <Text>
-                {hijraDateCorrection >= 0 ? "+" : "-"}
-                {hijraDateCorrection}
+                {daysCorrection! >= 0 ? "+" : "-"}
+                {daysCorrection}
               </Text>
               <Text>days</Text>
             </Pressable>
@@ -65,13 +98,5 @@ export default function Screen() {
         <Text>Version: 1.0.0</Text>
       </View>
     </View>
-  );
-}
-
-function ScreenLink({ href, title }: { href: Href; title: string }) {
-  return (
-    <Link href={href} className="p-3 my-1 rounded-lg bg-gray-200">
-      <Text>{title}</Text>
-    </Link>
   );
 }
